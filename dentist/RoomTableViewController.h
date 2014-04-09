11f580cd7a3200a14dbc3d0413348a7bf1b88e18
joy <joy32812@qq.com>
2014-04-09 14:14:40 +0800

    Room is difficult

---
+++ dentist/RoomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoomTableViewController.h
+//  dentist
+//
+//  Created by xiaoyuan wang on 4/8/14.
+//  Copyright (c) 2014 1010.am. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomTableViewController : UITableViewController
+
+@end
