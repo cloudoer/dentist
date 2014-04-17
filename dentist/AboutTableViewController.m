d5721f7f002a0b2fe0e4185550817498b3449d2f
joy <joy32812@qq.com>
2014-04-17 13:53:03 +0800

    about page

---
+++ dentist/AboutTableViewController.m
@@ -0,0 +1,43 @@
+//
+//  AboutTableViewController.m
+//  dentist
+//
+//  Created by xiaoyuan wang on 4/17/14.
+//  Copyright (c) 2014 1010.am. All rights reserved.
+//
+
+#import "AboutTableViewController.h"
+
+@interface AboutTableViewController ()
+
+@end
+
+@implementation AboutTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
