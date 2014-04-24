9aa7f84f276860b3506ef4c9a3d4e9700a5ffe60
joy <joy32812@qq.com>
2014-04-25 07:55:35 +0800

    amr done

--- dentist/Message/MessageListTableViewController.m
+++ dentist/Message/MessageListTableViewController.m
@@ -9,8 +9,13 @@
 #import "MessageListTableViewController.h"
 #import "AppDelegate.h"
 #import "MsgDetailViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "amrFileCodec.h"
+#import "GTMBase64.h"
 
-@interface MessageListTableViewController ()
+@interface MessageListTableViewController () <AVAudioRecorderDelegate,
+AVAudioSessionDelegate,
+AVAudioPlayerDelegate>
 
 @end
 
@@ -22,6 +27,12 @@
     NSArray *finalBuddyArray;
     
     NSString *clickedBareJIDStr;
+    
+    
+    NSMutableDictionary * _recorderSetting;
+    AVAudioPlayer       * _player;
+    AVAudioRecorder     * _recorder;
+    NSString            * _filePath;
 }
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -104,7 +115,44 @@
     {
 //        DDLogError(@"Error performing fetch: %@", error);
     }
+    
+    [self prepareToTestRecord];
 }
+
+- (void)prepareToTestRecord
+{
+    _recorderSetting = [[NSMutableDictionary alloc] init];
+    //数据格式
+    [_recorderSetting setObject:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+    //采样率
+    [_recorderSetting setObject:[NSNumber numberWithInt:8000] forKey:AVSampleRateKey];
+    //声道数目
+    [_recorderSetting setObject:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
+    //采样位数
+    [_recorderSetting setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    
+    _filePath = [NSTemporaryDirectory() stringByAppendingString:@"TestRecording.caf"];
+    NSError * _err = nil;
+    NSLog(@"file path = %@",_filePath);
+    _recorder = [[AVAudioRecorder alloc] initWithURL:[NSURL fileURLWithPath:_filePath]
+                                            settings:_recorderSetting
+                                               error:&_err];
+    _recorder.delegate = self;
+    _recorder.meteringEnabled = YES;
+    [_recorder prepareToRecord];
+    
+    
+    
+    AVAudioSession * _audioSession = [AVAudioSession sharedInstance];
+    NSError * _error = nil;
+    [_audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&_error];
+    if(_audioSession == nil)
+        NSLog(@"Error creating session: %@", [_error description]);
+    else
+        [_audioSession setActive:YES error:nil];
+    
+}
+
 
 - (void)getLatestMsgArray
 {
@@ -165,6 +213,123 @@
     if ([segue.identifier isEqualToString:@"MsgList2Detail"]) {
         MsgDetailViewController *controller = segue.destinationViewController;
         controller.bareJIDStr = clickedBareJIDStr;
+    }
+}
+- (IBAction)record:(UIBarButtonItem *)sender {
+    if (sender.tag == 0) {
+        sender.title = @"stop";
+        [self beginRecorder];
+    }else {
+        sender.title = @"record";
+        [self stopRecorder];
+    }
+    sender.tag = 1 - sender.tag;
+}
+
+- (void)beginRecorder
+{
+    if (![_recorder record])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"录音失败"
+                                                        message:nil
+                                                       delegate:self
+                                              cancelButtonTitle:@"取消"
+                                              otherButtonTitles:nil];
+        [alert show];
+        NSLog(@"录制失败");
+    }
+    
+}
+
+
+- (void)stopRecorder
+{
+    [_recorder stop];
+}
+- (IBAction)play:(UIBarButtonItem *)sender {
+    if (_player && _player.isPlaying) {
+        [_player stop];
+        return;
+    }
+    
+    NSString * _amrFilePath = [NSTemporaryDirectory() stringByAppendingString:@"TestRecording.amr"];
+    NSString * _wavFilePath = [NSTemporaryDirectory() stringByAppendingString:@"TestRecording.wav"];
+    NSData * _amrData = [NSData dataWithContentsOfFile:_amrFilePath];
+    NSData * _wavData = DecodeAMRToWAVE(_amrData);
+    [_wavData writeToFile:_wavFilePath atomically:YES];
+    
+    NSError * _error = nil;
+    _player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:_wavFilePath] error:&_error];
+    _player.delegate = self;
+    _player.volume = 1.0;
+    if (_player==nil)
+    {
+        NSLog(@"声音放送--%@",[_error description]);
+    }else
+    {
+        [_player prepareToPlay];
+    }
+    
+    
+    if (![_player play])
+    {
+        NSLog(@" wrong !!!!");
+    }else
+    {
+        
+    }
+    
+    [[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+
+}
+
+
+#pragma mark Record Delegate
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag
+{
+    _filePath = [NSTemporaryDirectory() stringByAppendingString:@"TestRecording.caf"];
+    NSData * _cafData = [NSData dataWithContentsOfFile:_filePath];
+    
+    UILabel * _cafSizeLabel = (UILabel *)[self.view viewWithTag:103];
+    [_cafSizeLabel setText:[NSString stringWithFormat:@"%@%.1fk",_cafSizeLabel.text,[_cafData length] / 1024.0]];
+    
+    NSString * _amrFilePath = [NSTemporaryDirectory() stringByAppendingString:@"TestRecording.amr"];
+    NSData * _amrData = EncodeWAVEToAMR(_cafData, 1, 16);
+    
+    UILabel * _amrSizeLabel = (UILabel *)[self.view viewWithTag:104];
+    [_amrSizeLabel setText:[NSString stringWithFormat:@"%@%.1fk",_amrSizeLabel.text,[_amrData length] / 1024.0]];
+    [_amrData writeToFile:_amrFilePath atomically:YES];
+    
+    NSString * amrDataBase64Str = [[NSString alloc] initWithData:[GTMBase64 encodeData:_amrData] encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"armString -> %@", amrDataBase64Str);
+    
+}
+
+#pragma mark AVAudioPlayer Delegate
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag;
+{
+    [[UIDevice currentDevice] setProximityMonitoringEnabled:NO];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+}
+
+//处理监听触发事件
+-(void)sensorStateChange:(NSNotificationCenter *)notification;
+{
+    //如果此时手机靠近面部放在耳朵旁，那么声音将通过听筒输出，并将屏幕变暗（省电啊）
+    if ([[UIDevice currentDevice] proximityState] == YES)
+    {
+        NSLog(@"Device is close to user");
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        
+    }
+    else
+    {
+        NSLog(@"Device is not close to user");
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
     }
 }
 
