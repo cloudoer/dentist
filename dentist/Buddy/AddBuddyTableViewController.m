150d2336e8c9a64bff6cddd7281455a542f81025
joy <joy32812@qq.com>
2014-04-28 08:20:44 +0800

    add UI

---
+++ dentist/Buddy/AddBuddyTableViewController.m
@@ -0,0 +1,49 @@
+//
+//  AddBuddyTableViewController.m
+//  dentist
+//
+//  Created by xiaoyuan wang on 4/28/14.
+//  Copyright (c) 2014 1010.am. All rights reserved.
+//
+
+#import "AddBuddyTableViewController.h"
+
+@interface AddBuddyTableViewController () <UITextFieldDelegate>
+
+@end
+
+@implementation AddBuddyTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)searchButtonPressed:(UIButton *)sender {
+    [self performSegueWithIdentifier:@"search2result" sender:self];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
