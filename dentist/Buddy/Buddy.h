b77560cd890d717bf615f7f30924df2e3c27ae86
joy <joy32812@qq.com>
2014-04-28 08:01:11 +0800

    buddy model

---
+++ dentist/Buddy/Buddy.h
@@ -0,0 +1,26 @@
+//
+//  Buddy.h
+//  dentist
+//
+//  Created by xiaoyuan wang on 4/28/14.
+//  Copyright (c) 2014 1010.am. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Buddy : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * uid;
+@property (nonatomic, retain) NSString * username;
+@property (nonatomic, retain) NSString * realname;
+@property (nonatomic, retain) NSString * phone;
+@property (nonatomic, retain) NSString * photoStr;
+@property (nonatomic, retain) NSString * gender;
+@property (nonatomic, retain) NSString * brand;
+@property (nonatomic, retain) NSString * jobTitle;
+@property (nonatomic, retain) NSString * desc;
+@property (nonatomic, retain) NSString * address;
+
+@end
---
+++ dentist/Buddy/Buddy.m
@@ -0,0 +1,25 @@
+//
+//  Buddy.m
+//  dentist
+//
+//  Created by xiaoyuan wang on 4/28/14.
+//  Copyright (c) 2014 1010.am. All rights reserved.
+//
+
+#import "Buddy.h"
+
+
+@implementation Buddy
+
+@dynamic uid;
+@dynamic username;
+@dynamic realname;
+@dynamic phone;
+@dynamic photoStr;
+@dynamic gender;
+@dynamic brand;
+@dynamic jobTitle;
+@dynamic desc;
+@dynamic address;
+
+@end
